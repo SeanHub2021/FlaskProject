fd3f99645fa2756eaa916342dd79fe81e07ffe34
SeanHub2021 <shbristol12@gmail.com>
2024-01-20 11:31:37 +0000

    unzip flask template, replace base html, update base html script and image sources

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask ##capital F on Flask indicates is a class name, import the Flask cask
+from flask import Flask, render_template 
+##capital F on Flask indicates is a class name, import the Flask cask
+##import render_template from the Framework
 
 
 app = Flask(__name__) ##create an instance of Flask and store it in a variable named app. Name of the package, __name__ is a built-in Python variable. Flask needs this to know where to look for templates and files. 
@@ -7,7 +9,22 @@ app = Flask(__name__) ##create an instance of Flask and store it in a variable n
 
 @app.route("/") ##a decorator starts with the @ symbol, its a way of wrapping functions. When we try to browse the root directory, by "/", it triggers the function
 def index(): ##create function Index
-    return "Hello, World"
+    return render_template("index.html") ##call the render_template we imported from Flask earlier. Flask expects it to be in a directory named Templates, so we create that.
+##leave 2 lines gap between functions to be PEP8 compliant
+
+
+@app.route("/about")##this view we name "/about" also then becoems a URL
+def about(): ##bind that to a view we will also call "about"
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact(): 
+    return render_template("contact.html")
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 if __name__ == "__main__":
     app.run(
